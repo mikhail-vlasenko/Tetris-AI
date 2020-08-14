02ce3e5a9fa9dd063be4fd2d63fa8dd72ed91cb4
alexnurin <44980361+alexnurin@users.noreply.github.com>
2020-08-14 19:51:13 +0300

    Recognition of the next figures

--- digit.py
+++ digit.py
@@ -5,17 +5,46 @@ from mss import mss
 
 dbug = 0
 
+d = dict()
+d[0] = (230, 228, 180)
+d[1] = (182, 228, 247)
+d[2] = (177, 99, 140)
+d[3] = (228, 177, 148)
+d[4] = (128, 180, 235)
+d[5] = (180, 161, 235)
+d[6] = (171, 240, 177)
+d[7] = (96, 45, 36)
 
-def pD(arr):
+
+def pD(arr, fsize=10):
     if dbug == 1:
         n = len(arr)
         # print(n)
-        fig = plt.figure(figsize=(10, 10))
+        fig = plt.figure(figsize=(fsize, fsize))
         for i in range(n):
             fig.add_subplot(1, n, i + 1)
             plt.imshow(arr[i])
     else:
         pass
+
+
+def cmp_pixel(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) + abs(p1[2] - p2[2])
+
+
+def get_figur(next_figure):
+    # pD((next_figure,), 5)
+    next_figure = next_figure[20:25, 15:20]
+    # pD((next_figure,))
+    for i in range(5):
+        for j in range(5):
+            p = next_figure[i, j][:3]
+            for k in range(7):
+                res = cmp_pixel(d[k], p)
+                # print(i, j, k, res)
+                if res < 15:
+                    return k
+    return -1
 
 
 def get_field():
