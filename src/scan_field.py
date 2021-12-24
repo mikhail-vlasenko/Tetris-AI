45dafa845ed872133c6345505576763e2bba2fed
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2021-12-25 00:08:13 +0300

    minor speed up with screen capture

--- src/scan_field.py
+++ src/scan_field.py
@@ -4,6 +4,8 @@ from mss import mss
 from config import CONFIG
 
 consts = CONFIG['display consts']
+screen_capture = mss()
+monitor = {"left": 0, "top": 0, "width": CONFIG['screen width'], "height": CONFIG['screen height']}
 
 
 def print_image(arr, figure_size=10):
@@ -66,23 +68,21 @@ def get_field() -> (np.array, int):
     takes a screenshot and computes playing grid
     :return: field grid, next piece id
     """
-    monitor = {"left": 0, "top": 0, "width": CONFIG['screen width'], "height": CONFIG['screen height']}
-    with mss() as sct:
-        img = np.array(sct.grab(monitor))
-        pixels = simplified(consts.get_field_from_screen(img))
+    img = np.array(screen_capture.grab(monitor))
+    pixels = simplified(consts.get_field_from_screen(img))
 
-        next_piece = get_figure_by_color(consts.get_next(img))
-        # all empty initially
-        field = np.zeros((20 + CONFIG['extra rows'], 10))
-        # find middles of grid cells
-        cell_size = pixels.shape[1] // 10
-        vertical_centers = np.array(np.linspace(cell_size // 2, pixels.shape[0] + cell_size // 2, 21 + CONFIG['extra rows'])[:-1], int)
-        horizontal_centers = np.array(np.linspace(cell_size // 2, pixels.shape[1] + cell_size // 2, 11)[:-1], int)
-        # go through all cell centers
-        for i, v in enumerate(vertical_centers):
-            for j, h in enumerate(horizontal_centers):
-                if pixels[v][h] == 1:
-                    # check a nearby pixel to ensure its not a snowflake or a star
-                    if pixels[v+5][h+5] == 1:
-                        field[i][j] = 1
-        return field, next_piece
+    next_piece = get_figure_by_color(consts.get_next(img))
+    # all empty initially
+    field = np.zeros((20 + CONFIG['extra rows'], 10))
+    # find middles of grid cells
+    cell_size = pixels.shape[1] // 10
+    vertical_centers = np.array(np.linspace(cell_size // 2, pixels.shape[0] + cell_size // 2, 21 + CONFIG['extra rows'])[:-1], int)
+    horizontal_centers = np.array(np.linspace(cell_size // 2, pixels.shape[1] + cell_size // 2, 11)[:-1], int)
+    # go through all cell centers
+    for i, v in enumerate(vertical_centers):
+        for j, h in enumerate(horizontal_centers):
+            if pixels[v][h] == 1:
+                # check a nearby pixel to ensure its not a snowflake or a star
+                if pixels[v+5][h+5] == 1:
+                    field[i][j] = 1
+    return field, next_piece
